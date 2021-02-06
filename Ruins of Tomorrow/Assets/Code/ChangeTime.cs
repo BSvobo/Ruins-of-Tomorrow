973b184575d3a71545c770ce01609a98a7460d44
Braden Svoboda <braden.svoboda@gmail.com>
2021-02-06 16:18:25 -0600

    Added some time stuff, haven't tested it yet tho
    need to be able to interact with stuff first
    
    also it looks like there are a TON of other files that changed shit?

---
+++ Ruins of Tomorrow/Assets/Code/ChangeTime.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTime : MonoBehaviour
+{
+    Collider[] Affectables;
+    public static Timeable.timeState LocalTimeState = Timeable.timeState.Present;
+    // Start is called before the first frame update
+    void Start()
+    {
+         Affectables = Physics.OverlapSphere(GetComponent<Transform>().position, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void sendThemBack()
+    {
+        int objects_affected = 0;
+
+        //Get the time state we want to change the radius to
+        Timeable.timeState toChange = newTime();
+
+        //Change the time state of all of the (affectable) objects in the radius
+        foreach (Collider collider in Affectables)
+        {
+            var iter = collider.gameObject.GetComponent<Timeable>();
+            if (!iter)
+            {
+                continue;
+            }
+            else
+            {
+                objects_affected += 1;
+                iter.changeTime(toChange);
+            }
+        }
+
+        LocalTimeState = toChange;
+    }
+
+    Timeable.timeState newTime()
+        {
+            if (LocalTimeState == Timeable.timeState.Present)
+            {
+                return Timeable.timeState.Past;
+            }
+            else
+            {
+                return Timeable.timeState.Present;
+            }
+        }
+}
---
+++ Ruins of Tomorrow/Assets/Code/Timeable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timeable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum timeState { Past, Present};
+    timeState currentTime = timeState.Present;
+    Sprite pastSprite;
+    Sprite presentSprite;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Actually add stuff here to change the state of the object as necessary, this just makes it actually affected by the change in time
+    //Should probably write more functions to change sprite, interactability/collider, etc.
+    public void changeTime(timeState new_time)
+    {
+        if(currentTime == new_time){
+            return;
+        }
+        else
+        {
+            if(new_time == timeState.Present)
+            {
+                currentTime = timeState.Past;
+            }
+            else
+            {
+                currentTime = timeState.Present;
+            }
+        }
+    }
+}
