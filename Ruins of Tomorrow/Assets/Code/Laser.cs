6009ce1b8dc4955a72351b361c58013fe9d5963d
annikadev <61029585+annikadev@users.noreply.github.com>
2021-02-09 18:44:25 -0600

    we got lasers now folx

---
+++ Ruins of Tomorrow/Assets/Code/Laser.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer l;
+    private Vector3 gun;
+    public Vector2 dir;
+    private Timeable _timeable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        l = gameObject.GetComponent<LineRenderer>();
+        gun = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeable = gameObject.GetComponent<Timeable>();
+        
+        if (_timeable.getTimeState() == Timeable.timeState.Past)
+        {
+            LaserRender();
+        }
+    }
+
+    void LaserRender()
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(gun, dir);
+        RaycastHit2D hit = hits[1];
+        
+        Vector3 hitPoint = hit.point;
+        List<Vector3> pos = new List<Vector3>(); 
+        pos.Add(gun);
+        pos.Add(hitPoint);
+        l.startWidth = .1f; 
+        l.endWidth = .1f;
+        l.SetPositions(pos.ToArray());
+        l.useWorldSpace = true;
+            
+        if (hit.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+    }
+    
+    
+}
